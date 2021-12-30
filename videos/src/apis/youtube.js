2b39466465c928f70dd58e381ca2522127c67d4e
ctaylor <ctaylr13@gmail.com>
2021-12-30 15:24:04 -0500

    update key

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyB7tIB0m6LO2SB4ib-xxZg8tEAQYnKPCpc';
+// const KEY = 'AIzaSyB7tIB0m6LO2SB4ib-xxZg8tEAQYnKPCpc';
+const KEY = 'AIzaSyCPPSwTAi-bXzOC8CZ1rmPfoAwT3GCnDaU';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
